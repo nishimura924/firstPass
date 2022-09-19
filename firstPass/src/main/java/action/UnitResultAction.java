57c959225db45a8913e3cfcfa0c6de4237b933d7
mori-tfc <113403276+mori-tfc@users.noreply.github.com>
2022-09-20 08:56:19 +0900

    修正（森下）

--- firstPass/src/main/java/action/UnitResultAction.java
+++ firstPass/src/main/java/action/UnitResultAction.java
@@ -9,13 +9,11 @@ import java.util.List;
 
 public class UnitResultAction extends Action
 {
+	@SuppressWarnings("unchecked")
 	public String execute(HttpServletRequest request,HttpServletResponse response)throws Exception
 	{ 
 		//セッションを取得、生成
 		HttpSession session = request.getSession();
-		
-		//ResultActionよりセッション「実績サマリ」を取得
-		List<SummaryOfResult> summary=(List<SummaryOfResult>)session.getAttribute("summary");
 		
 		//問題数
 		int questionCount = 0;
@@ -23,29 +21,36 @@ public class UnitResultAction extends Action
 		int correctCount = 0;
 		//正答率
 		int correctRate = 0;
+				
+		//セッション「実績サマリ」を取得
+		List<SummaryOfResult> summary=(List<SummaryOfResult>)session.getAttribute("summary");
+		//難易度の情報を取得
 		
-		//Listの長さ（問題数）をカウントアップ
-		for(int i=0; i<=summary.size(); i++)
+		if(summary != null)
 		{
-			if(summary.get(i).getCorrect().equals("1"))
+			//Listの長さ（問題数）をカウントアップ
+			for(int i=0; i<=summary.size(); i++)
 			{
-				correctCount++;
+				questionCount++;
+				if(summary.get(i).getCorrect().equals("1"))
+				{
+					correctCount++;
+				}
 			}
+			correctRate = correctCount/summary.size();
 		}
-		correctRate = correctCount/summary.size();
-		
-		//最後の問題の解答時刻を取得
-		//Date answerDate = summary.get(summary.size()-1).;
-		
-		
-		//request.setAttribute("answerDate", answerDate);
-		//問題数をリクエストで取得
-		request.setAttribute("questionCount",summary.size());
-		//正答数をリクエストで取得
-		request.setAttribute("correctCount", correctCount);
-		//正答率をリクエストで取得
-		request.setAttribute("correctRate", correctRate);
-		//jspへ渡す
-		return "unitResult.jsp";
+			//最後の問題の解答時刻を取得
+			//result answerDate = summary.get(summary.size()-1).;
+				
+				
+			//request.setAttribute("answerDate", answerDate);
+			//問題数をリクエストで取得
+			request.setAttribute("questionCount",questionCount);
+			//正答数をリクエストで取得
+			request.setAttribute("correctCount", correctCount);
+			//正答率をリクエストで取得
+			request.setAttribute("correctRate", correctRate);
+				
+			return "unitResult.jsp";
 	}
 }
--- firstPass/src/main/webapp/action/unitResult.jsp
+++ firstPass/src/main/webapp/action/unitResult.jsp
@@ -1,18 +1,19 @@
 <%@page contentType="text/html; charset=UTF-8" %>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@page import="bean.User,java.util.List" %>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
 	</head>
+	<body>
 <c:choose>
 	<c:when test="${user.userName == null}">ゲスト</c:when>
 	<c:otherwise>${user.userName }</c:otherwise>
 </c:choose>
-さんの今回のテスト結果（難易度：${Result.difficulty}）
+<form>
+さんの今回のテスト結果（難易度：<p>${Result.difficulty}</p>)
 
-実施日：${answerDate}
+実施日：<p></p>
 <br>
 出題数:${questionCount}問
 <br>
@@ -20,7 +21,9 @@
 <br>
 正答率：${correctRate}％
 <br>
-
+</form>
+</body>
+</html>
 
 
 <%@include file="footer_menu.jsp" %>
