22677bc8a95704e15e35031149703bd0275b5017
mori-tfc <113403276+mori-tfc@users.noreply.github.com>
2022-09-22 12:01:47 +0900

    questionDAO統合による修正

--- firstPass/src/main/java/action/AllResultQuestionAction.java
+++ firstPass/src/main/java/action/AllResultQuestionAction.java
@@ -6,7 +6,7 @@ package action;
 * @version　1.0.0
 */
 
-import dao.QuestionDAOmorishita;
+import dao.QuestionDAO;
 import tool.Action;
 import javax.servlet.http.*;
 import java.util.List;
@@ -16,7 +16,7 @@ public class AllResultQuestionAction extends Action
 {	
 	public String execute(HttpServletRequest request,HttpServletResponse response)throws Exception
 	{
-		QuestionDAOmorishita dao = new QuestionDAOmorishita();
+		QuestionDAO dao = new QuestionDAO();
 		
 		//DB内の分野項目を取得する
 		List<String> genreList = dao.getGenre();
