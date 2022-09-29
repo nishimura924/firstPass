cf2e7dc5e3b3044b09e1c7fb9212c25d0f6fa05c
研修生03 <03@WSIY1222330H>
2022-09-29 13:00:20 +0900

    ValidCheck修正(ー－―‐)

--- firstPass/src/main/java/action/ValidCheck.java
+++ firstPass/src/main/java/action/ValidCheck.java
@@ -10,7 +10,7 @@ public class ValidCheck
 	//ユーザー名の入力値チェック（半角・全角英数字、漢字・かな・カナ、記号（！？ー～＿＊※＠）１００文字以内）
 	public static boolean validUserName(String userName)
 	{
-		return userName.matches("[0-9０-９a-zA-Zａ-ｚＡ-Ｚあ-んア-ン一-龠々～~_＿？?！!※＊*＠@]{1,100}");
+		return userName.matches("[0-9０-９a-zA-Zａ-ｚＡ-Ｚあ-んア-ンー－―‐龠々～~_＿？?！!※＊*＠@]{1,100}");
 	}
 	//ユーザーパスワードの入力値チェック（半角英数字、８文字以上）
 	public static boolean validUserPass(String userPass)
@@ -21,6 +21,6 @@ public class ValidCheck
 	//コメントの入力値チェック（半角・全角英数字、漢字・かな・カナ、記号（！？ー～＿＊＠）５００文字以内）
 	public static boolean validComment(String comment)
 	{
-		return comment.matches("[0-9０-９a-zA-Zａ-ｚＡ-Ｚあ-んア-ン一-龠々～~_＿？?！!※＊*＠@]{1,500}");
+		return comment.matches("[0-9０-９a-zA-Zａ-ｚＡ-Ｚあ-んア-ンー－―‐龠々～~_＿？?！!※＊*＠@]{1,500}");
 	}
 }
